e2f361afa11c20e2ae1915171221084e64128aa3
Dave T <74991341+davethomson2000@users.noreply.github.com>
2024-03-27 21:54:38 +0000

    finish adding gui, refactor helper functions getRoundWinner and resetForNewGame

--- app.js
+++ app.js
@@ -3,14 +3,18 @@ const container = document.querySelector("#container")
 const rockButton = document.querySelector("#rock")
 const paperButton = document.querySelector("#paper")
 const scissorsButton = document.querySelector("#scissors")
+const playerScoreText = document.querySelector("#player-score")
+const computerScoreText = document.querySelector("#computer-score")
+const winnerText = document.querySelector("#winner")
+
 let playerWins = 0;
 let computerWins = 0;
-let roundsPlayer = 0;
-let overallWinner;
+let roundsPlayed = 0;
+let overallWinner = "";
 
-rockButton.addEventListener("click", (event) => {playRound(playerChoice = getPlayerChoice(event.target.id), computerChoice = getComputerChoice())})
-paperButton.addEventListener("click", (event) => {playRound(playerChoice = getPlayerChoice(event.target.id), computerChoice = getComputerChoice())})
-scissorsButton.addEventListener("click", (event) => {playRound(playerChoice = getPlayerChoice(event.target.id), computerChoice = getComputerChoice())})
+rockButton.addEventListener("click", (event) => { playRound(playerChoice = getPlayerChoice(event.target.id), computerChoice = getComputerChoice()) })
+paperButton.addEventListener("click", (event) => { playRound(playerChoice = getPlayerChoice(event.target.id), computerChoice = getComputerChoice()) })
+scissorsButton.addEventListener("click", (event) => { playRound(playerChoice = getPlayerChoice(event.target.id), computerChoice = getComputerChoice()) })
 
 function getComputerChoice() {
     let selection;
@@ -28,7 +32,21 @@ function getPlayerChoice(selection) {
     return selection;
 };
 
-function playRound(playerChoice, computerChoice) {
+function resetGame() {
+    //clean up
+
+    playerWins = 0
+    computerWins = 0
+    roundsPlayed = 0
+    playerScoreText.textContent = playerWins
+    computerScoreText.textContent = computerWins
+    winnerText.textContent = ""
+    while (container.firstChild)
+        container.removeChild(container.firstChild)
+    
+}
+
+function getRoundWinner(playerChoice, computerChoice) {
     let roundWinner;
     if (playerChoice === computerChoice) {
         roundWinner = "Tie"
@@ -37,26 +55,43 @@ function playRound(playerChoice, computerChoice) {
         (playerChoice === "rock" && computerChoice === "scissors")
         || (playerChoice === "scissors" && computerChoice === "paper")
         || (playerChoice === "paper" && computerChoice === "rock")
-        ) {
-            roundWinner = "Player"
-        }
-        else {
-            roundWinner = "Computer"
-        };
-    
-    console.log("Winner was: " + roundWinner)    
-    const div = document.createElement("div")
-    div.textContent = `Player choice: ${playerChoice} | Computer choice: ${computerChoice} | Round winner: ${roundWinner}`
-    container.appendChild(div)
-    
+    ) {
+        roundWinner = "Player"
+        playerWins++
+    }
+    else {
+        roundWinner = "Computer"
+        computerWins++
+    };
     return roundWinner
 }
 
+function playRound(playerChoice, computerChoice) {
+    if (roundsPlayed === 5)
+        resetGame()
+
+    let roundWinner = getRoundWinner(playerChoice, computerChoice)
+    console.log("Winner was: " + roundWinner)
+
+    const div = document.createElement("div")
+    div.textContent = `Player choice: ${playerChoice} | Computer choice: ${computerChoice} | Round winner: ${roundWinner}`
+    container.appendChild(div)
+
+    roundsPlayed++
+    playerScoreText.textContent = playerWins
+    computerScoreText.textContent = computerWins
+
+    //check if game is complete
+    getOverallWinner(playerWins, computerWins)
+
+ 
+}
+
 function playGame() {
+    //deprecated now the GUI is available
     for (let i = 1; i <= 5; i++) {
         console.log("Round: " + i + ". Please choose.")
-        switch (playRound (getPlayerChoice(), getComputerChoice()))
-        {
+        switch (playRound(getPlayerChoice(), getComputerChoice())) {
             case "Player":
                 playerWins++
                 break
@@ -65,17 +100,18 @@ function playGame() {
                 break
         }
     }
-    
-    console.log("Player wins: " + playerWins) 
-    console.log("Computer wins: " + computerWins) 
 
+    console.log("Player wins: " + playerWins)
+    console.log("Computer wins: " + computerWins)
     overallWinner = getOverallWinner(playerWins, computerWins)
 
     console.log("Overall winer: " + overallWinner)
 }
 
 function getOverallWinner(playerWins, computerWins) {
-    let overallWinner
+    if (roundsPlayed < 5)
+        return
+
     if (playerWins > computerWins) {
         overallWinner = "Player"
     }
@@ -83,8 +119,11 @@ function getOverallWinner(playerWins, computerWins) {
         overallWinner = "Computer"
     }
     else {
-        overallWinner = "Computer"
+        overallWinner = "Tie"
     }
+
+    winnerText.textContent = `Overall Winner: ${overallWinner}!`
+
     return overallWinner
 }
 
